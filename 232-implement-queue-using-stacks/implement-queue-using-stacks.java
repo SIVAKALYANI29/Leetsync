d6c56795bba4cd38d62ad1060ba2651cb455b63a
Siva kalyani Balla <sivakalyaniballa@gmail.com>
2026-08-11 15:10:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (96.99%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+class MyQueue {
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+
+    public MyQueue() {}
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        moveElements();
+        return stack2.pop();
+    }
+
+    public int peek() {
+        moveElements();
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    private void moveElements() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+    }
+}
+
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
